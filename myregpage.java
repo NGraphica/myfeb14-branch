d42d00438289a631c9d33aa2d9e53cfe5be3f98b
Omkar Hendre <ngraphica2020@gmail.com>
2022-07-23 16:00:51 +0530

    developer work is completed

--- myregpage.java
+++ myregpage.java
@@ -1,2 +1,7 @@
 This is Java registration page/file
 Manager works is completed
+1st day of java lines of code
+2nd day of java lines of code 
+3rd day of java lines of code
+4th day of java lines of code here 90% of task is completed 
+5th day of java lines of code 
